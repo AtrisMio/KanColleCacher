d2d64d386c9d66041fdba44d023b74aefb36808d
AtrisMio <mio512056026@gmail.com>
2016-01-16 18:45:18 +0800

    更改api_start2修改方式

--- KanColleCacher/ImageModifier.cs
+++ KanColleCacher/ImageModifier.cs
@@ -2,6 +2,7 @@
 using d_f_32.KanColleCacher;
 using d_f_32.KanColleCacher.Configuration;
 using Fiddler;
+using System.Linq;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -30,55 +31,8 @@ namespace Gizeta.KanColleCacher
         {
             FiddlerApplication.BeforeRequest -= Image_BeforeRequest;
             FiddlerApplication.BeforeResponse -= Image_BeforeResponse;
-
-            ModifyData.Items.Clear();
             jsonData = "";
         }
-        /*
-        private void setModifiedData(ModifyData data)
-        {
-            string graphStr = Regex.Match(jsonData, @"\{([^{]+?)" + data.FileName + @"([^}]+?)\}").Groups[0].Value;
-            string sortNo = Regex.Match(graphStr, @"api_sortno"":(\d+)").Groups[1].Value;
-            string infoStr = Regex.Match(jsonData, @"\{([^{]+?)api_sortno"":" + sortNo + @"([^}]+?)\}").Groups[0].Value;
-
-            var graphReplaceStr = graphStr;
-            var infoReplaceStr = infoStr;
-
-            var temp = data.Data["ship_name"];
-            if (temp != null && temp.Length > 0)
-            {
-                infoReplaceStr = Regex.Replace(infoReplaceStr, @"api_name"":""(.+?)""", @"api_name"":""" + temp + @"""");
-            }
-
-            var modList = new string[] { "boko_n", "boko_d",
-                                         "kaisyu_n", "kaisyu_d",
-                                         "kaizo_n", "kaizo_d",
-                                         "map_n", "map_d",
-                                         "ensyuf_n", "ensyuf_d",
-                                         "ensyue_n",
-                                         "battle_n", "battle_d",
-                                         "weda", "wedb" };
-            foreach (var mod in modList)
-            {
-                if(!data.Data.ContainsKey("boko_n_left")) break;
-
-                temp = data.Data[mod + "_left"];
-                if (temp != null && temp.Length > 0)
-                {
-                    graphReplaceStr = Regex.Replace(graphReplaceStr, mod + @""":\[([\d-]+),([\d-]+)\]", mod + @""":[" + temp + @",$2]");
-                }
-
-                temp = data.Data[mod + "_top"];
-                if (temp != null && temp.Length > 0)
-                {
-                    graphReplaceStr = Regex.Replace(graphReplaceStr, mod + @""":\[([\d-]+),([\d-]+)\]", mod + @""":[$1," + temp + @"]");
-                }
-            }
-
-            jsonData = jsonData.Replace(graphStr, graphReplaceStr);
-            jsonData = jsonData.Replace(infoStr, infoReplaceStr);
-        }
-        */
         private void Image_BeforeRequest(Session oSession)
         {
             if (!set.CacheEnabled) return;
@@ -91,87 +45,31 @@ namespace Gizeta.KanColleCacher
 
         private void Image_BeforeResponse(Session oSession)
         {
-            //if (!set.CacheEnabled) return;
+            if (!set.CacheEnabled) return;
 
             if (oSession.PathAndQuery.StartsWith("/kcsapi/api_start2") && Settings.Current.FurnitureHackEnabled)
             {
                 jsonData = oSession.GetResponseBodyAsString();
-                var head = jsonData.Substring(0, jsonData.IndexOf("\"api_mst_furnituregraph")+25);
-                var tail = jsonData.Substring(jsonData.IndexOf(",\"api_mst_useitem"));
-                var api_start2 = DynamicJson.Parse(jsonData.Substring(jsonData.IndexOf("api_mst_furniture") + 19, jsonData.IndexOf("api_mst_furnituregraph") - jsonData.IndexOf("api_mst_furniture") - 21));
-                var api_start1 = DynamicJson.Parse(jsonData.Substring(7));
-                ArrayList cotain = new ArrayList();
-                ArrayList api_mst_furnituregraph = new ArrayList();
-                foreach (var image in api_start1["api_data"]["api_mst_furnituregraph"])
+                var head = jsonData.Substring(0,jsonData.IndexOf("],\"api_mst_useitem"));
+                var tail = jsonData.Substring(jsonData.IndexOf("],\"api_mst_useitem"));
+                var api_mst_furnituregraph = jsonData.Substring(jsonData.IndexOf("api_mst_furnituregraph") + 24, jsonData.IndexOf("api_mst_useitem") - jsonData.IndexOf("api_mst_furnituregraph") - 26);
+                List<string> contain = new List<string>();
+                var furnituregraph_info = new Regex(@"{""api_id"":([0-9]+?).*?}").Matches(api_mst_furnituregraph);
+                foreach(Match result in furnituregraph_info)
                 {
-                    api_mst_furnituregraph.Add(new { api_id = image["api_id"], api_type = image["api_type"], api_no = image["api_no"],api_filename = ((int)(image["api_no"] + 1)).ToString("d3"), api_version = image["api_version"] });
-                    cotain.Add(image["api_id"]);
+                    contain.Add(result.Groups[1].Value);
                 }
-                
-                foreach (var image in api_start2)
+                var furniture_info = new Regex(@"({""api_id"":)([0-9]+?)(,""api_type"":[0-9]+?,""api_no"":)([0-9]+?),""api_title.*?}").Matches(jsonData);
+                foreach(Match result in furniture_info)
                 {
-                    if (!cotain.Contains(image["api_id"]))
-                        api_mst_furnituregraph.Add(new { api_id = image["api_id"], api_type = image["api_type"], api_no = image["api_no"], api_filename = ((int)(image["api_no"] + 1)).ToString("d3"), api_version = "1" });
-                    System.Console.WriteLine(image["api_title"]);
+                    if (!contain.Contains(result.Groups[2].Value))
+                    {
+                        var fur_file = result.Groups[1].Value + result.Groups[2].Value + result.Groups[3].Value + result.Groups[4].Value + @",""api_filename"":""" + (int.Parse(result.Groups[4].Value) + 1).ToString("d3") + @""",""api_version"":""1""}";
+                        head += "," + fur_file;
+                    }
                 }
-                //api_start2["api_data"].api_mst_furniture = api_mst_furnituregraph.ToArray();
-                var str = DynamicJson.Serialize(api_mst_furnituregraph.ToArray());
-                oSession.utilSetResponseBody(head+str+tail);
-                 //api_mst_furnituregraph
-
-                    //ModifyData.Items.ForEach(x => setModifiedData(x));
-                    //oSession.utilSetResponseBody(jsonData);
+                oSession.utilSetResponseBody(head + tail);
             }
         }
     }
-    /*
-    internal class ModifyData
-    {
-        public static List<ModifyData> Items = new List<ModifyData>();
-
-        internal ModifyData(string path)
-        {
-            var st = path.LastIndexOf('\\') + 1;
-            var ed = path.LastIndexOf(".config.ini");
-            if (st > 0 && ed > st)
-            {
-                this.FileName = path.Substring(st, ed - st);
-            }
-            else
-            {
-                this.FileName = "Unknown";
-            }
-
-            this.Data = new Dictionary<string,string>();
-            var parser = ConfigParser.ReadIniFile(path);
-            if (parser["info"] != null)
-            {
-                this.Data.Add("ship_name", parser["info"]["ship_name"]);
-            }
-            else
-            {
-                this.Data.Add("ship_name", null);
-            }
-            if (parser["graph"] != null)
-            {
-                var modList = new string[] { "boko_n", "boko_d",
-                                             "kaisyu_n", "kaisyu_d",
-                                             "kaizo_n", "kaizo_d",
-                                             "map_n", "map_d",
-                                             "ensyuf_n", "ensyuf_d",
-                                             "ensyue_n",
-                                             "battle_n", "battle_d",
-                                             "weda", "wedb" };
-                foreach (var mod in modList)
-                {
-                    this.Data.Add(mod + "_left", parser["graph"][mod + "_left"]);
-                    this.Data.Add(mod + "_top", parser["graph"][mod + "_top"]);
-                }
-            }
-        }
-
-        public string FileName { get; set; }
-        public Dictionary<string, string> Data { get; set; }
-    }
-    */
 }
